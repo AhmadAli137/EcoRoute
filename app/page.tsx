c5d45863f30bcaad1bf43c8a3fc290cc71ca7f90
AhmadAli137 <ahmad100307@hotmail.com>
2024-01-25 14:29:26 -0500

    Made Map Page more responsive

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,9 @@ import SlidersComponent from "./SidersComponent";
 
 const MapPage = () => {
   return (
-    <div className="flex flex-col  px-5">
-      <div className="flex">
-        <div className="flex-grow bg-slate-100 shadow-xl rounded-lg overflow-hidden border border-green-600">
+    <div className="flex flex-col px-5">
+      <div className="md:flex">
+        <div className="flex-grow bg-slate-100 shadow-xl rounded-lg overflow-hidden border border-green-600 mb-4 md:mb-0 md:mr-10">
           <div className="map-container p-4">
             <h2 className="text-2xl text-center font-semibold text-green-700 mb-4">
               Map
@@ -16,18 +16,19 @@ const MapPage = () => {
           </div>
         </div>
 
-        <div className="w-1/4 flex flex-col ml-10">
+        <div className="md:w-1/4">
           {/* Input Box */}
-          <div className="bg-slate-100 shadow-xl overflow-hidden p-4 mb-4 flex flex-col items-center justify-center border border-green-600 rounded-lg">
+          <div className="bg-slate-100 shadow-xl overflow-hidden p-4 mb-4 border border-green-600 rounded-lg">
             <h2 className="text-2xl font-semibold text-green-700 mb-4">
               Input Parameters
             </h2>
-            <div className="w-full max-w-md px-4 py-2 bg-white rounded-lg shadow-inner">
+            <div className="w-full max-w-md mx-auto px-4 py-2 bg-white rounded-lg shadow-inner">
               <SlidersComponent />
             </div>
           </div>
+
           {/* Output Box */}
-          <div className="text-center bg-slate-100 shadow-xl overflow-hidden p-4 flex-grow border border-green-600 rounded">
+          <div className="text-center bg-slate-100 shadow-xl overflow-hidden p-4 border border-green-600 rounded-lg">
             <h2 className="text-2xl font-semibold text-green-700 mb-4">
               Output
             </h2>
